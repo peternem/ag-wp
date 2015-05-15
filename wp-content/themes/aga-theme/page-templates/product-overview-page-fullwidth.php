41397497ab89ec095e1249c1924244be39c3894d
peternem <mjpeternell@gmail.com>
2015-05-14 19:32:28 -0700

    First AG Dev commit

---
+++ wp-content/themes/aga-theme/page-templates/product-overview-page-fullwidth.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: Product Overview Page
+ *
+ * This is the template that displays full width page without sidebar
+ *
+ * @package sparkling
+ */
+
+get_header(); ?>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+	<div class="jumbotron">
+	 <?php the_post_thumbnail( 'sparkling-full-width', array( 'class' => 'single-featured img-responsive' )); ?>
+	
+      <div class="jumbo-caption">
+           
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+       <?php if(function_exists('the_subtitle')) {
+       	?>
+       	<p><?php echo the_subtitle();?></p>
+       	<?php 
+       }
+       ?> 
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
+    </div>
+  	<div id="primary" class="content-area container-fluid">
+
+    	<main id="main" class="site-main" role="main">
+
+	      <?php while ( have_posts() ) : the_post(); ?>
+	
+	        <?php get_template_part( 'content', 'home-page' ); ?>
+	        
+	      <?php endwhile; // end of the loop. ?>
+
+    	</main><!-- #main -->
+
+	</div><!-- #primary -->
+	<div class="container-fluid">
+		<!-- Example row of columns -->
+		<div class="row">
+			<div class="col-md-4">
+				<h2>Heading</h2>
+				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h2>Heading</h2>
+				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h2>Heading</h2>
+				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+		</div>
+  	</div>
+  
+
+<?php get_footer(); ?>
