a1034cf3e4d7281368ca6c12ccf0ed4803da6caa
MJPeternell <mjpeternell@gmail.com>
2019-02-08 18:48:24 -0800

    .gitignore is now working

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1542324053;s:7:"authKey";s:64:"*-7@Pf*$^24E]]L)wO@ (fVyYs=1p>yI#.7wv^YMa`M{ljL0Liy3)o$RKnZL$tO2";s:7:"version";s:5:"1.0.4";}
