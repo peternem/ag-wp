81073a81fee60ef5d3cdfc6175f6ef7fa48aa654
MJPeternell <mjpeternell@gmail.com>
2019-02-08 18:50:04 -0800

    .gitignore is now working

--- wp-content/plugins/admin-custom-login/admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined( 'ABSPATH' ) or die();
-require_once WEBLIZAR_ACL_PLUGIN_DIR_PATH_FREE . '/admin/WL_ACL_FREE_Menu.php';
-?>
--- wp-content/plugins/wp-social-widget/assets/js/social-color_picker.js
+++
@@ -1,5 +0,0 @@
-function social_color_picker(){
-	
-        // Add Color Picker to all inputs that have 'color-field' class
-        jQuery( '.color-field' ).wpColorPicker();
-}
